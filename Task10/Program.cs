0eb9c7b53b4921bbbda41f5e0fea75ba3582e9af
Makc <m_stefanenkov@mail.ru>
2023-01-23 14:36:40 +0300

    Решил задачу 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+	Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+	и на выходе показывает вторую цифру этого числа. 
+	Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+	456 -> 5
+	782 -> 8
+	918 -> 1
+	*/
+	
+
+	int GetNumber(int number)
+	{
+	    int firstNumber = number / 10;
+	    int secondNumber = firstNumber % 10;
+	    if (secondNumber < 0)
+	    {
+	        secondNumber = (secondNumber) * -1;
+	    }
+	
+
+	    int result = secondNumber;
+	    return result;
+	}
+	
+
+	Console.Write("Введите трехзначное число: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	if (number < 1000)
+	{
+	    int finishNumber = GetNumber(number);
+	    Console.WriteLine($"Вторая цифра введенного числа => {finishNumber}");
+	}
+	else
+	{
+	    Console.WriteLine($"Вы ввели не трехзначное число");
+	}
